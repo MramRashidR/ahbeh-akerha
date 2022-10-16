1a2d8787959894f59f2388628b1ede318c9b947e
MramRashidR <mramrashid98@gmail.com>
2022-10-16 09:33:05 +0300

    set up ejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const  PORT = 3000
+
+
+app.set("view wngine" , "ejs")
+
+
+
+app.listen(PORT , ()=>{
+    console.log("You are running the ahbeh-akerha app")
+})
